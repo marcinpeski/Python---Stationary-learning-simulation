07bf882fb212f390baa91172fe7099814a76a36c
Marcin Peski <63936653+marcinpeski@users.noreply.github.com>
2024-04-22 18:12:24 -0400

    clean up generate beliefs

--- generate beliefs.py
+++ generate beliefs.py
@@ -5,16 +5,14 @@
 #THis script draws three plots (given n= 3, 4, or 5): 
 #   Plot of transition functions,
 #   plot of beliefs for different values of pi,
-
-
-n_alphas = 400
-n_pis = 400
+n_alphas = 200
+n_pis = 200
+n_xs = 101
 
 epsilon = 1e-4
 delta = 1e-3
 pi = 0.8
-first_pi = 0.51
-
+first_pi = 0.52
 
 
 import numpy as np
@@ -32,62 +30,50 @@ def g_1(x, pi=0.55, alpha=0.3):
 def psi(x, k, n):
     return x**k*(1-x)**(n-k)
 
-def alpha_range(pi):
-    #g_0<1
+def generate_alphas(pi):
     if pi <2/3:
         alpha_min = 1 - 1/(3*(1-pi))
     else: 
-        alpha_min = (2-3*pi)/(4-3*pi)
+        alpha_min = 2/3*pi - 1
     alpha_max = 1 - 1/3/pi
-    #g_1<1
-
-    return alpha_min+delta, alpha_max-delta
+    alpha_min, alpha_max = alpha_min + delta, alpha_max - delta
+    alphas_first = np.linspace(alpha_min, alpha_max, n_alphas)
+    if alpha_min<=0<=alpha_max:
+        alphas_first = np.append(alphas_first, 0)
+    alphas = []
+    for alpha in alphas_first:
+        g = partial(g_1, pi=pi, alpha = alpha)
+        e = epsilon
+        fail = False 
+        while e<1-epsilon and not fail:
+            e_old, e = e, g(e)
+            fail = abs(e-e_old)<0.001*epsilon
+        if not fail:
+            alphas.append(alpha)
+    return alphas
 
 def compute_beliefs_for_alpha(pi=0.55, n=3, alpha = 0.3):
-#generate uniform grid from epsilon to 1-epsilon
+    print(f'computing pi = {pi}, alpha = {alpha}', end='\r')
     g = partial(g_1, pi=pi, alpha = alpha)
     g1_epsilon = g(epsilon)
-    x = np.linspace(epsilon, g1_epsilon, 101)
-    e = epsilon
+    x = np.linspace(epsilon, g1_epsilon, n_xs)
     data = [x]
-    all_xs = x
-    fail = False
-    k = 0
-    while e<1-epsilon and not fail:
-        e_old, e = e, g(e)
-        fail = abs(e-e_old)<0.001*epsilon
+    while g(x[0])<1-epsilon:
         x = g(x)
         data += [x]
-        all_xs = np.append(all_xs, x)
-        k += 1
-    if not fail:
-        psi_data = {k: [psi(x, k, n) for x in data] for k in range(1,n)}
-        psi_sum = {k:np.sum(psi_data[k], axis=0) for k in psi_data}
-        p1 = psi_sum[1]/(psi_sum[1]+psi_sum[2])
-        p1_range = (min(p1), max(p1))
-        g0 = 3*(max(0,-alpha)+(1-pi)*(1-abs(alpha)))
-        g1 = 3*(max(0,-alpha)+pi*(1-abs(alpha)))
-        p0 = -np.log(g0)/(-np.log(g0)+np.log(g1))
-        return p0, p1_range
-    else:
-        return None
-
-def compute_beliefs(pi):
-    alpha_min, alpha_max = alpha_range(pi)
-    alpha_min, alpha_max = alpha_min + delta, alpha_max - delta
-    alphas = np.linspace(alpha_min, alpha_max, n_alphas)
-    if alpha_min<=0<=alpha_max:
-        alphas = np.append(alphas, 0)
-    beliefs = {}
-    for alpha in alphas: 
-        print(f'computing pi = {pi}, alpha = {alpha}', end='\r')
-        beliefs[alpha] =compute_beliefs_for_alpha(pi, alpha=alpha)
-    return beliefs
-
+    psi_data = {k: [psi(x, k, n) for x in data] for k in range(1,n)}
+    psi_sum = {k:np.sum(psi_data[k], axis=0) for k in psi_data}
+    p1 = psi_sum[1]/(psi_sum[1]+psi_sum[2])
+    p1_range = (min(p1), max(p1))
+    g0 = 3*(max(0,-alpha)+(1-pi)*(1-abs(alpha)))
+    g1 = 3*(max(0,-alpha)+pi*(1-abs(alpha)))
+    p0 = -np.log(g0)/(-np.log(g0)+np.log(g1))
+    return p0, p1_range
+    
+#generate beliefs
 pis = np.linspace(first_pi, 1, n_pis)
-beliefs = {pi:None for pi in pis}
-for pi in pis:
-    beliefs[pi] = compute_beliefs(pi)
+beliefs = {pi:{alpha:compute_beliefs_for_alpha(pi, alpha=alpha) for alpha in generate_alphas(pi)} for pi in pis}
+
 #save beliefs to file
 import pickle
 with open('beliefs.pkl', 'wb') as f:
